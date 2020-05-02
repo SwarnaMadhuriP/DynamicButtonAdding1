44eec8fcc122f1a1df354705064d8b479ae39f25
SwarnaMadhuriP <swarnap.cs17@gmail.com>
2020-05-02 14:48:30 +0530

    Added one more activity

---
+++ app/src/main/java/com/example/dynamicbuttonadding/Case1Details.java
@@ -0,0 +1,14 @@
+package com.example.dynamicbuttonadding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Case1Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_case1_details);
+    }
+}
--- app/src/main/java/com/example/dynamicbuttonadding/MainActivity.java
+++ app/src/main/java/com/example/dynamicbuttonadding/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
@@ -21,12 +22,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       db=openOrCreateDatabase("ComplaintRegistrationDB", Context.MODE_PRIVATE, null);
+        db=openOrCreateDatabase("ComplaintRegistrationDB", Context.MODE_PRIVATE, null);
         layout=findViewById(R.id.linearlayoutid);
-      //  Cursor c =db.rawQuery("select count(*) from CaseRegistration",null);
-        //int test = c.getCount();
-       // long test = DatabaseUtils.queryNumEntries(db, "table_name");
-        for(int i=1;i<=5;i++){
+       // db.execSQL("CREATE TABLE IF NOT EXISTS  CaseRegistration(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,caseId VARCHAR,Status VARCHAR,Type1 VARCHAR,VName VARCHAR,CName VARCHAR,Complaintname VARCHAR,Mobile VARCHAR,Place VARCHAR,Date1 VARCHAR,Time1 VARCHAR,assigned INT);");
+        db.execSQL("INSERT INTO CaseRegistration (Status,Type1,VName,CName,Complaintname,Mobile,Place,Date1,Time1,assigned) VALUES('" +"recorded"+"','" +"robbery"+"','"+"mukesh"+"','"+"swarna"+"','"+"car robbed"+"','"+"8105504284"+"','"+"delhi"+"','"+"1/05/2020"+"','"+"14" + "',0);");
+        Cursor c =db.rawQuery("select count(*) from CaseRegistration",null);
+        int test = c.getCount();
+        int count = 0;
+        if(null != c){
+            if(c.getCount() > 0){
+                c.moveToFirst();
+                count = c.getInt(0);
+            }
+        }
+       //long test = DatabaseUtils.queryNumEntries(db, "table_name");
+        for(int i=1;i<=count;i++){
             final Button button=new Button(this);
             button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             button.setId(i);
@@ -37,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(getApplicationContext(),"case no pressed is "+finalI,Toast.LENGTH_SHORT).show();
+                    Intent gotocase=new Intent(MainActivity.this,Case1Details.class);
+                    startActivity(gotocase);
+
                 }
             });
             layout.addView(button);
